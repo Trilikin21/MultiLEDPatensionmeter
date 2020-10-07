4a4779dbf0b983dfcb1fa5090650b7607356fa05
Jeremy Forest <1910182@champlaincollege.qc.ca>
2020-10-07 13:03:22 -0400

    photosensor now will determine blink speed of LED's instead of power off all LED'S

--- Lab4.ino
+++ Lab4.ino
@@ -4,12 +4,12 @@ const int yellowLED = 7;
 const int greenLED = 5;
 const int blueLED = 3;
 const int whiteLED = 4;
-const int sensorPin = A0; 
+const int sensorPin = A0;
 const int ldrPin = A1;
 
 void setup()
 {
-  
+
   Serial.begin(9600);
   pinMode(yellowLED, OUTPUT);
   pinMode(redLED, OUTPUT);
@@ -19,62 +19,66 @@ void setup()
   pinMode(ldrPin, INPUT);
   pinMode(sensorPin, INPUT);
 }
- 
+
 void loop()
 {
 
- //get's sensor value from the photoresistor
- int ldrStatus = analogRead(ldrPin);
- //if the photoresitor value is over 150, continue with inner if...else statement
- if (ldrStatus < 150) {
-    //get's sensor value from the potentionmeter
-    int sensorValue = analogRead(sensorPin);
-    
-    //devide by 4 and -5 to normalize maximize the values to 250 to assure function with if..else statements below
-    sensorValue = (sensorValue/4)-5;
-    //delay of 0.1 seconds
-    delay(100);
-    Serial.println("LDR IS DARK, ACTIVATE LED");
-    //depending on the value, it will light up different LED'S
-     if (sensorValue <=50) {
+  //get's sensor value from the photoresistor
+  int ldrStatus = analogRead(ldrPin);
+  //if the photoresitor value is over 150, continue with inner if...else statement
+  Serial.print(ldrStatus);
+
+  //get's sensor value from the potentionmeter
+  int sensorValue = analogRead(sensorPin);
+
+  int blinkSpeed = ldrStatus * 100;
+
+  //devide by 4 and -5 to normalize maximize the values to 250 to assure function with if..else statements below
+  sensorValue = (sensorValue / 4) - 5;
+  //delay of 0.1 seconds
+  delay(100);
+  Serial.println("LDR IS DARK, ACTIVATE LED");
+  //depending on the value, it will light up different LED'S
+  if (sensorValue < 50) {
     digitalWrite(yellowLED, HIGH);
+    delay(blinkSpeed);
+    digitalWrite(yellowLED, LOW);
   }
-     else {
-      digitalWrite(yellowLED, LOW);
+  else {
+    digitalWrite(yellowLED, LOW);
   }
-  if (sensorValue >50 && sensorValue <=100 ) {
-      digitalWrite(redLED, HIGH);
+  if (sensorValue > 50 && sensorValue <= 100 ) {
+    digitalWrite(redLED, HIGH);
+    delay(blinkSpeed);
+    digitalWrite(redLED, LOW);
   }
-    else {
-      digitalWrite(redLED, LOW);
+  else {
+    digitalWrite(redLED, LOW);
   }
-  if (sensorValue >100 && sensorValue <=150) {
-      digitalWrite(greenLED, HIGH);
+  if (sensorValue > 100 && sensorValue <= 150) {
+    digitalWrite(greenLED, HIGH);
+    delay(blinkSpeed);
+    digitalWrite(greenLED, LOW);
   }
-    else {
-      digitalWrite(greenLED, LOW);
+  else {
+    digitalWrite(greenLED, LOW);
   }
-  if (sensorValue >150 && sensorValue <=200) {
-      digitalWrite(whiteLED, HIGH);
+  if (sensorValue > 150 && sensorValue <= 200) {
+    digitalWrite(whiteLED, HIGH);
+    delay(blinkSpeed);
+    digitalWrite(whiteLED, LOW);
   }
-    else {
-      digitalWrite(whiteLED, LOW);
+  else {
+    digitalWrite(whiteLED, LOW);
   }
-  if (sensorValue >200 && sensorValue <=250) {
-      digitalWrite(blueLED, HIGH);
+  if (sensorValue > 200 && sensorValue <= 250) {
+    digitalWrite(blueLED, HIGH);
+    delay(blinkSpeed);
+    digitalWrite(blueLED, LOW);
   }
-    else {
-      digitalWrite(blueLED, LOW);
+  else {
+    digitalWrite(blueLED, LOW);
+
   }
   //depending on the value, it will light up different LED'S
- }
- else {
-  //if not, deactivate all lights.
-  Serial.println("LDR IS BRIGHT, DEACTIVATE LED");
-  digitalWrite(blueLED, LOW);
-  digitalWrite(whiteLED, LOW);
-  digitalWrite(greenLED, LOW);
-  digitalWrite(redLED, LOW);
-  digitalWrite(yellowLED, LOW);
- }
 }
